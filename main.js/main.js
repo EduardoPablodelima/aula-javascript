b8cb71464e328e95a658be8dccb4028cd03ccc56
Eduardo Pablo de lima <62775928+EduardoPablodelima@users.noreply.github.com>
2021-06-23 22:07:21 -0300

    adicionandojava

---
+++ main.js/main.js
@@ -0,0 +1,25 @@
+var d = new Date("com este comando eu consigo inporta a data, hora e minutos para a minha aplicação.");
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes())
+
+
+
+
+/* 
+var idade = prompt("Qual a sua idade") // comando prompt abre uma caixa de dialago para a pergunata QUAL A SUA IDADE e faz executar o comando.
+if (idade >= 18){
+    alert("você já é de maior pode entra");
+}
+else{
+    alert("Você ainda não pode está aqui");
+}
+
+
+
+/* var boas = "Seja Bem vindo." 
+var nome = "Eduardo Pablo"
+alert (boas + nome)*/
+// lista.pushn("com esse comando eu adiciono elementos dentro da minha lista")
+// lista.pop ("com esta comando eu retiro elemento de dentro da minha lista ")
+// console.log(lista.join("diciono espaço ou traço na minha lista "))
